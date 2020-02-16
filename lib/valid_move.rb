72c8f00613d67faba865e8e1ccdee1ed3058d470
Crystal Schooling <bytesizemagic@gmail.com>
2020-02-16 11:21:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
+  index = index.between(0, 8)
   if position_taken? == false #&& index.between(0, 8)
     return true
   elsif position_taken? == true #&& index.between(0, 8)
